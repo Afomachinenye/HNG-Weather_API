e885ed49667825058fe939f232fbfad643eb92df
Afomachinenye <Afomachukwuchinenyeonyekaba@gmail.com>
2024-07-03 21:50:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const requestIp = require("request-ip");
+const indexRoute = require("./route");
+
+const app = express();
+app.use(requestIp.mw());
+
+app.use("/api/hello", indexRoute);
+
+app.get("/", (req, res) => {
+  res.json("Homepage, Welcome!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(process.env.WEATHERSTACK_API_KEY);
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { getWeatherByIP } = require("./services/weather");
+const { resolveClientIp } = require("./services/ip");
+const { buildResponse } = require("./utils/responseBuilder");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const clientIp = resolveClientIp(req);
+  const name = req.query.visitor_name || "Guest";
+
+  try {
+    const data = await getWeatherByIP(clientIp);
+    const location = data.location ? data.location.name : "Unknown";
+    const temperature = data.current ? data.current.temperature : "Unknown";
+
+    const response = buildResponse(clientIp, name, location, temperature);
+    res.json(response);
+  } catch (error) {
+    console.error("Error in fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
